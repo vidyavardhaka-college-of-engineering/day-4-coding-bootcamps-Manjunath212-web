6a6a363c5ea62b35d6bb21a8a0074c24133d9e28
Manjunath212web <>
2021-05-28 06:58:04 +0000

    array

--- Program_3.c
+++ Program_3.c
@@ -9,7 +9,22 @@
 #include<stdio.h> 
 int main()
 {
-  //Declare all the variables[row, col, array a(2D array), i] used in the code
+    int arr1[100], arr2[100];
+    int i, n;
+	
+	
+       printf("\n\nCopy the elements one array into another array :\n");
+       printf("----------------------------------------------------\n");
+	   
+       printf("Input the number of elements to be stored in the array :");
+       scanf("%d",&n);
+   
+       printf("Input %d elements in the array :\n", n);
+       for( i=0;i<n; i++)
+        {
+	      printf(" element in the array : \n" ); 
+	      scanf("%d", &arr1[i]);
+	    }//Declare all the variables[row, col, array a(2D array), i] used in the code
    //Read the Value of row and coloumn of the matrix (i.e size of matrix) using scanf statement
    //Read the elements of matrix a using nested for loop and scanf statment
    //Print the elements of matrix a using nested for loop and printf statment as per the above specified format
